332515ff20954e2b35d4c460e29dc50eabba7db4
Elliot Winkler <elliot.winkler@gmail.com>
2020-06-13 23:01:46 -0600

    Update Node provision to work with asdf

--- lib/setup_script_generator/templates/provisions/node.sh
+++ lib/setup_script_generator/templates/provisions/node.sh
@@ -1,23 +1,29 @@
 provision-node() {
-  if [[ -f .node-version ]]; then
+  if [[ -f .tool-versions ]]; then
+    REQUIRED_NODE_VERSION=$(cat .tool-versions | grep '^nodejs ' | sed -Ee 's/^nodejs (.+)$/\1/')
+  elif [[ -f .node-version ]]; then
     REQUIRED_NODE_VERSION=$(cat .node-version)
   elif [[ -f .nvmrc ]]; then
     REQUIRED_NODE_VERSION=$(cat .nvmrc)
   else
     error "You don't seem to have a Node version set in your project."
     print-wrapped "\
-You'll need to create a .node-version or .nvmrc file in your project before you
-can run this script.
-    "
+You'll need to create either a .tool-versions file or .nvmrc file in your
+project before you can run this script."
     exit 1
   fi
 
   ensure-node-installed
-  install-node-dependencies
+  ensure-project-node-dependencies-installed
 }
 
 ensure-node-installed() {
-  if has-executable nodenv; then
+  if has-executable asdf; then
+    if ! (asdf current nodejs | grep $REQUIRED_NODE_VERSION'\>' &>/dev/null); then
+      banner "Installing Node $REQUIRED_NODE_VERSION with asdf"
+      asdf install nodejs $REQUIRED_NODE_VERSION
+    fi
+  elif has-executable nodenv; then
     if ! (nodenv versions | grep $REQUIRED_NODE_VERSION'\>' &>/dev/null); then
       banner "Installing Node $REQUIRED_NODE_VERSION with nodenv"
       nodenv install --skip-existing "$REQUIRED_NODE_VERSION"
@@ -31,19 +37,16 @@ ensure-node-installed() {
   else
     error "You don't seem to have a Node manager installed."
     print-wrapped "\
-We recommend using nodenv. You can find instructions to install it here:
+We recommend using asdf. You can find instructions to install it here:
 
-    https://github.com/nodenv/nodenv#installation
+    https://asdf-vm.com
 
-Make sure to follow the instructions to configure your shell so that nodenv is
-automatically loaded.
-
-When you're done, open up a new terminal tab and re-run this script."
+When you're done, close and re-open this terminal tab and re-run this script."
     exit 1
   fi
 }
 
-install-node-dependencies() {
+ensure-project-node-dependencies-installed() {
   banner 'Installing Node dependencies'
 
   if [[ -f package-lock.json ]]; then
@@ -53,9 +56,8 @@ install-node-dependencies() {
   else
     error "Sorry, I'm not sure how to install your dependencies."
     print-wrapped "\
-You'll need to create a package-lock.json or yarn.lock file in your project
-before you can run this script.
-    "
+Are you missing a package.json? Have you run 'npm install' or 'yarn install'
+yet?"
     exit 1
   fi
 }

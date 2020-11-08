b431ef63b75097188061da9373a791485d28d4c0
Elliot Winkler <elliot.winkler@gmail.com>
2020-11-07 23:56:13 -0700

    Fix Ruby provision so Bundler 1.x is installed correctly
    The syntax for installing specific versions of gems seems to have
    changed in a recent version of RubyGems: instead of `gem install GEM -v
    VERSION`, you now use `gem install GEM:VERSION`.

--- lib/setup_script_generator/templates/provisions/ruby.sh
+++ lib/setup_script_generator/templates/provisions/ruby.sh
@@ -73,7 +73,7 @@ ensure-project-ruby-dependencies-installed() {
   banner 'Installing Ruby dependencies'
 
   if [[ $USE_BUNDLER_1 -eq 1 ]] && (! has-bundler || ! [[ $(bundle -v) =~ '^Bundler version 1\.' ]]); then
-    gem install bundler -v '~> 1.0'
+    gem install bundler:'~> 1.0'
   elif ! has-bundler; then
     gem install bundler
   fi

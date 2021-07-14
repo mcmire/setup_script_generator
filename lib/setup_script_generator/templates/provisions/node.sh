73a06bc3dcd0af79b98e2b2c37b0a910b9fe11b2
Elliot Winkler <elliot.winkler@gmail.com>
2021-07-14 17:34:34 -0600

    Ensure Yarn is installed

--- lib/setup_script_generator/templates/provisions/node.sh
+++ lib/setup_script_generator/templates/provisions/node.sh
@@ -10,7 +10,7 @@ provision-node() {
   fi
 
   if [[ -z $REQUIRED_NODE_VERSION ]]; then
-    error "Could not determine required Node version for this project."
+    error 'Could not determine required Node version for this project.'
     print-wrapped "\
 Your project needs to include either a valid .tool-versions file with a 'nodejs'
 line or a valid .node-version or .nvimrc file."
@@ -51,17 +51,24 @@ When you're done, close and re-open this terminal tab and re-run this script."
 }
 
 ensure-project-node-dependencies-installed() {
-  banner 'Installing Node dependencies'
-
   if [[ -f package-lock.json ]]; then
+    banner 'Installing Node dependencies'
     npm install
   elif [[ -f yarn.lock ]]; then
+    if ! type yarn &>/dev/null; then
+      banner 'Installing Yarn 1'
+      npm install -g yarn
+    fi
+    banner 'Installing Node dependencies'
     yarn install
   else
     error "Sorry, I'm not sure how to install your dependencies."
     print-wrapped "\
-Are you missing a package.json? Have you run 'npm install' or 'yarn install'
-yet?"
+It doesn't look like you have a package-lock.json or yarn.lock in your project
+yet. I'm not sure which package manager you plan on using, so you'll need to run
+either \`npm install\` or \`yarn install\` once first. Additionally, if you want
+to use Yarn 2, then now is the time to switch to that. Then you can re-run this
+script."
     exit 1
   fi
 }
